9230df41c688b4d82c77a9659f3983c304bb6ac0
tram888 <bigtram888@gmail.com>
2023-08-14 20:45:33 +0900

    Add mail checker

---
+++ utils.py
@@ -0,0 +1,11 @@
+import re
+
+from aiogram import types
+from aiogram.dispatcher.filters import Filter
+
+
+class EmailCheck(Filter):
+    key = 'is_valid_email'
+    pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    async def check(self, msg: types.Message):
+        return self.pattern.match(msg.text)
