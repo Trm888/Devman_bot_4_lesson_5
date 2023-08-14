5a529a65550cecfdd27ab4791f284cbe230bee2b
tram888 <bigtram888@gmail.com>
2023-08-14 21:30:12 +0900

    Edit pep8

--- utils.py
+++ utils.py
@@ -7,5 +7,6 @@ from aiogram.dispatcher.filters import Filter
 class EmailCheck(Filter):
     key = 'is_valid_email'
     pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
     async def check(self, msg: types.Message):
         return self.pattern.match(msg.text)
