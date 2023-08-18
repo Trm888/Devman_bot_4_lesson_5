32d6f016dde407f11ddf557c310f4c3d3ff5f631
tram888 <bigtram888@gmail.com>
2023-08-18 16:17:14 +0900

    Delete get_redis_storage_for_state function

--- loader.py
+++ loader.py
@@ -2,14 +2,11 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 
-def get_redis_storage_for_state(host, port, redis_password):
-    return RedisStorage2(host=host, port=port, password=redis_password)
-
 
 def get_bot(bot_token):
     return Bot(bot_token, parse_mode=types.ParseMode.HTML)
 
 
 def get_dispatcher(bot, host, port, redis_password):
-    storage = get_redis_storage_for_state(host, port, redis_password)
+    storage = RedisStorage2(host=host, port=port, password=redis_password)
     return Dispatcher(bot, storage=storage)
